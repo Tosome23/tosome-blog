33b65b53fd6ab6e82aa30a5d0643020174eac221
Tosome23 <23logis@naver.com>
2025-08-09 16:30:43 +0900

    .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -4,16 +4,25 @@ module.exports = function(eleventyConfig) {
   // static 폴더를 사이트 루트로 복사
   eleventyConfig.addPassthroughCopy({"static/style.css": "style.css"});
 
+  // 날짜 포맷 필터
   eleventyConfig.addFilter("date", d =>
     DateTime.fromJSDate(d).toFormat("yyyy-LL-dd")
   );
 
+  // 블로그 포스트 컬렉션
+  eleventyConfig.addCollection("posts", function(collection) {
+    return collection.getFilteredByGlob("content/blog/*.md");
+  });
+
   return {
-    dir: { input: ".", includes: "layouts", data: "_data", output: "_site" },
+    dir: { 
+      input: ".", 
+      includes: "layouts", 
+      data: "_data", 
+      output: "_site" 
+    },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     templateFormats: ["md","njk","html"]
   };
-    eleventyConfig.addCollection("posts", function(collection) {
-  return collection.getFilteredByGlob("content/blog/*.md");
 };
